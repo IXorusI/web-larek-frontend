20f5a8e6878596ee6474f21872ad13fce9cbf371
IXorusI <astartes93@gmail.com>
2024-03-10 20:18:39 +0300

    Много изменений, вытащит и отобразил карточки, модалки вроде работают

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import './scss/styles.scss';
 
+import {Api} from "./components/base/api";
+import {EventEmitter} from "./components/base/events";
+import {API_URL, CDN_URL} from "./utils/constants";
+
 console.log('test')
 
